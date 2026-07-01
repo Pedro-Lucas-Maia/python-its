9da77f108ceed5b1376d06cb28bf66c428fbf160
Pedro-IoT <pedromaiadepaiva@gmail.com>
2026-07-01 00:25:12 -0300

    Muda a lingua do site para pt-br

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Providers>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             {children}
           </ThemeProvider>
         </Providers>
